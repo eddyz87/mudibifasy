9bfedb3b4c93bbe00b137353c92bd43767a22504
Igor <igor.myth@gmail.com>
2013-08-10 16:02:54 +0300

    Remove was-fold param from choose functions

--- src/choose.lisp
+++ src/choose.lisp
@@ -112,7 +112,7 @@
           (cdr args)
           :initial-value (car args)))
 
-(defun construct-term (size vars op-set was-fold)
+(defun construct-term (size vars op-set)
   (if (= size 1)
       (choose-one (append (list 0 1)
                           vars))
@@ -120,7 +120,7 @@
        (choose-do
          op <- (choose-one (intersection op-set
                                          '(not shl1 shr1 shr4 shr16)))
-         sub-term <- (construct-term (1- size) vars op-set was-fold)
+         sub-term <- (construct-term (1- size) vars op-set)
          (choose-return (list op sub-term)))
        (if (<= size 2)
            (fail)
@@ -129,8 +129,8 @@
                                              '(and or xor plus)))
              sz <- (choose-one (loop for i from 1 to (- size 2)
                                    collect i))
-             sub-term1 <- (construct-term sz vars op-set was-fold)
-             sub-term2 <- (construct-term (- size sz 1) vars op-set was-fold)
+             sub-term1 <- (construct-term sz vars op-set)
+             sub-term2 <- (construct-term (- size sz 1) vars op-set)
              (choose-return
               (list op sub-term1
                     sub-term2))))
@@ -142,46 +142,44 @@
                                    collect i))
              sz1 <- (choose-one (loop for i from 1 to (- sz 1)
                                     collect i))
-             sub-term-t <- (construct-term sz1 vars op-set was-fold)
-             sub-term-f <- (construct-term (- sz sz1) vars op-set was-fold)
-             sub-term-c <- (construct-term (- size sz 1) vars op-set was-fold)
+             sub-term-t <- (construct-term sz1 vars op-set)
+             sub-term-f <- (construct-term (- sz sz1) vars op-set)
+             sub-term-c <- (construct-term (- size sz 1) vars op-set)
              (choose-return
               (list 'if0 
                     sub-term-c
                     sub-term-t
                     sub-term-f))))
        (if (or (<= size 4)
-               was-fold
-               (not (or (member 'fold op-set)
-                        (member 'tfold op-set))))
+               (not (member 'fold op-set)))
            (fail)
-           (choose-do
-             sz <- (choose-one (loop for i from 2 to (- size 3)
-                                   collect i))
-             sz1 <- (choose-one (loop for i from 1 to (- sz 1)
+           (let ((no-fold-set (remove 'fold op-set)))
+             (choose-do
+               sz <- (choose-one (loop for i from 2 to (- size 3)
                                     collect i))
-             sub-term-1 <- (construct-term sz1 vars op-set t)
-             sub-term-2 <- (construct-term (- sz sz1) vars op-set t)
-             v1 <- (choose-one (cons 'x vars))
-             v2 <- (choose-one (cons 'z vars))
-             sub-term-b <- (construct-term (- size sz 2) 
-                                            (append vars (list v1 v2))
-                                            op-set
-                                            t)
-             (choose-return
-              (list 'fold
-                    sub-term-1
-                    sub-term-2
-                    (list 'lambda (list v1 v2)
-                          sub-term-b))))))))
+               sz1 <- (choose-one (loop for i from 1 to (- sz 1)
+                                     collect i))
+               sub-term-1 <- (construct-term sz1 vars no-fold-set)
+               sub-term-2 <- (construct-term (- sz sz1) vars no-fold-set)
+               v1 <- (choose-one (cons 'x vars))
+               v2 <- (choose-one (cons 'z vars))
+               sub-term-b <- (construct-term (- size sz 2) 
+                                             (append vars (list v1 v2))
+                                             no-fold-set)
+               (choose-return
+                (list 'fold
+                      sub-term-1
+                      sub-term-2
+                      (list 'lambda (list v1 v2)
+                            sub-term-b)))))))))
 
 (defun construct-program-1 (size op-set)
   (choose-do
     term <- (if (member 'tfold op-set)
                 (choose-do
-                  t1 <- (construct-term (- size 5) '(x y) (remove 'tfold op-set) nil)
+                  t1 <- (construct-term (- size 5) '(x y) (remove 'tfold op-set))
                   (choose-return `(fold x 0 (lambda (x y) ,t1))))
-                (construct-term (1- size) '(x) op-set nil))
+                (construct-term (1- size) '(x) op-set))
     (let ((progr `(lambda (x)
                     ,term)))
       (let ((ops (bv-operators progr)))
